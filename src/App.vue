e83004ead4273f8d41bc8d557f4f85a989cbc668
hivesbee <hivesbee@gmail.com>
2019-10-01 23:33:55 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" @click="append">要素を追加</button>
+    <div class="flex">
+      <ul>
+        <li v-for="(x, i) in items" :key="i">
+          <span>{{ x }},{{ gen(i) }}</span>
+        </li>
+      </ul>
+      <ul>
+        <li v-for="(x, i) in items" :key="i">
+          <list-item :x="x" :i="i" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListItem from './components/ListItem.vue'
 
+/* eslint-disable */
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ListItem
+  },
+  data () {
+    return {
+      items: ['a', 'a', 'a']
+    }
+  },
+  computed: {
+    gen () {
+      console.log('reactive call anonymous')
+      return (x) => {
+        console.log('reactive call ' + x)
+        return `test${x}`
+      }
+    },
+    comp () {
+
+    }
+  },
+  methods: {
+    append () {
+      this.items.push('a')
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.flex {
+  display: flex;
 }
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>{{x}},{{ gen }}</span>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'ListItem',
+  props: {
+    x: {
+      type: String,
+      required: true
+    },
+    i: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    gen () {
+      console.log('[ListItem] reactive call' + this.i)
+      return `test${this.i}`
+    }
+  }
+}
+</script>
